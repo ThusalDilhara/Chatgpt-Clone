56561551d9b8e9cc8b33c9c5faeaebe21f3ef593
Thusal Dilhara <thusaldilhara20@gmail.com>
2025-04-02 11:22:05 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import SideBar from './Components/sideBar'  
 import MainPage from './Components/mainPage'
 import Navbar from './Components/navbar'
+import SignIn from './Components/signIn'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   
@@ -9,11 +11,21 @@ function App() {
 
   return (
     <>
-    <div className='flex'>
-      <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
-      <Navbar isOpen={isOpen}/>
-      <MainPage />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <div className='flex'>
+                <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
+                <Navbar isOpen={isOpen} />
+                <MainPage />
+              </div>
+            }
+          />
+          <Route path="/" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
