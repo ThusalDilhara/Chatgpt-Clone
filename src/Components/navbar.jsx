379a2f5527ffa4310b7fa2095c440bc1adbd3a04
Thusal Dilhara <thusaldilhara20@gmail.com>
2025-04-03 09:53:29 +0530

    display first two letters in profile icon done

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserCircle2 } from 'lucide-react';
 import ShinyText from './shinyText';
+import { account } from '../config/appwriteConfig';
 
 const Navbar = ({ isOpen }) => {
+
+  const [userInitial,setUserInital]=useState("");
+
+
+  useEffect(()=>{
+     const getUser=async()=>{
+        
+      try{
+        const user= account.get();
+
+        if((await user).name){
+          const username= (await user).name.slice(0,2).toUpperCase();
+          setUserInital(username);
+        }
+
+      }catch(error){
+         console.log("Error fetching user data:", error);
+         
+      }
+     };
+      getUser();
+  },[]);
   return (
     <>
       <div className='flex justify-between items-center bg-[#282828] p-4 h-[57px] w-full fixed z-10 border-b border-[#303030]'>
@@ -11,10 +34,15 @@ const Navbar = ({ isOpen }) => {
           <ShinyText text="ChatGPT" disabled={false} speed={5} className='custom-class' />
         </div>
 
-        <div className="flex items-center gap-2 cursor-pointer hover:opacity-80 transition-opacity">
+        {userInitial?
+          <div className="w-8 h-8 flex items-center justify-center rounded-full bg-pink-400  text-white text-sm font-light ">
+          {userInitial}
+        </div>
+        
+        :<div className="flex items-center gap-2 cursor-pointer hover:opacity-80 transition-opacity">
           <UserCircle2 size={32} strokeWidth={1.5} className="text-[#B5B5B5] hover:text-[#929292]" />
           
-        </div>
+        </div>}
 
       </div>
     </>
