3c5fde4162b453892204b21a2f61526a2761beeb
Thusal Dilhara <thusaldilhara20@gmail.com>
2025-04-20 10:33:55 +0530

    change

--- src/config/appwriteConfig.jsx
+++ src/config/appwriteConfig.jsx
@@ -1,7 +1,7 @@
 import { Client, Account } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1") 
+  .setEndpoint("https://cloud.appwrite.io/v1") 
   .setProject("67eabd47002f05d1b097");
 
 const account = new Account(client);
